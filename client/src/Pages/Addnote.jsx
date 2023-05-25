0c326774ab57f93b4c3a1b5ea45a41a697ed7842
anshid haneef <anshidhaneefa710@gmail.com>
2023-05-25 06:30:54 +0530

    User side Edit

--- client/src/Pages/Addnote.jsx
+++ client/src/Pages/Addnote.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import {TextField , Button} from '@mui/material'
 
 const Addnote = () => {
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Handle form submission logic here
+    };
+  
+  
   return (
     <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aperiam rem ea
-      temporibus debitis?
+    <form onSubmit={handleSubmit}>
+    <TextField label="Name" variant="outlined" fullWidth />
+    <TextField label="Email" variant="outlined" fullWidth />
+    <TextField
+      label="Message"
+      variant="outlined"
+      multiline
+      rows={4}
+      fullWidth
+    />
+    <Button type="submit" variant="contained" color="primary">
+      Submit
+    </Button>
+  </form>
     </>
   );
 };
--- client/src/Pages/EditNote.jsx
+++ client/src/Pages/EditNote.jsx
@@ -1,106 +1,80 @@
-// import React from 'react'
+import { TextField, Button, Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import config from "../config.json";
+import axios from "axios";
 
-// const editNote = () => {
-//   return (
-//     <div>
-        
-//     </div>
-//   )
-// }
+import { useParams,useNavigate } from "react-router-dom";
 
-// export default editNote
+const EditNote = () => {
+  const navigate = useNavigate()
+  const { id } = useParams();
 
+  console.log(id);
 
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-// import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
+  const [post, setPost] = useState({
+    title: "",
+    body: "",
+  });
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
-    padding: theme.spacing(2),
-  },
-  '& .MuiDialogActions-root': {
-    padding: theme.spacing(1),
-  },
-}));
+  useEffect(() => {
+    if (!id) return;
+    const fetchPost = async () => {
+      const { data } = await axios.get(`${config.apiUrl}/${id}`);
+      setPost(data);
+    };
+    fetchPost();
+  },[id]);
 
-function BootstrapDialogTitle(props) {
-  const { children, onClose, ...other } = props;
+  const handleChange = (e) =>{
+      const postClone = {... post}
+      postClone[e.target.name] = e.target.value
+      setPost(postClone)
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    
+    if( id == 'new'){
+      await axios.post(`${config.apiUrl}/${id}`,post)
+      return navigate('/')
+    }else{
+      await axios.put(`${config.apiUrl}/${id}`,post)
+      return navigate('/')
+    }
+
+  }
 
   return (
-    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-      {children}
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          {/* <CloseIcon /> */}
-        </IconButton>
-      ) : null}
-    </DialogTitle>
+    <>
+      <Box m={3}>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            type="text"
+            name="title"
+            value={post.title}
+            variant="outlined"
+            onChange={handleChange}
+            fullWidth
+          />
+          <TextField
+          type="text"
+          name="body"
+            variant="outlined"
+            value={post.body}
+            multiline
+            onChange={handleChange}
+            rows={4}
+            fullWidth
+          />
+          <Button type="submit" onClick={handleSubmit}
+            variant="contained" 
+             color="primary">
+            { id === 'new' ? 'post' : 'update' }
+          </Button>
+        </form>
+      </Box>
+    </>
   );
-}
-
-BootstrapDialogTitle.propTypes = {
-  children: PropTypes.node,
-  onClose: PropTypes.func.isRequired,
 };
 
-export default function CustomizedDialogs() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open dialog
-      </Button>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
-          Modal title
-        </BootstrapDialogTitle>
-        <DialogContent dividers>
-          
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </Typography>
-         
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Save changes
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
-    </div>
-  );
-}
-
-
+export default EditNote;
